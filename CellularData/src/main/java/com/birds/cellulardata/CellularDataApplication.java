93367b0d5d72af801c4cdbeff12b59bc74e33383
Tae <kangtae27@gmail.com>
2021-08-29 18:00:28 -0700

    TK: Initial Commit

---
+++ CellularData/src/main/java/com/birds/cellulardata/CellularDataApplication.java
@@ -0,0 +1,13 @@
+package com.birds.cellulardata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CellularDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CellularDataApplication.class, args);
+	}
+
+}
